3903b4ccdafb38fb6e97d5f2e60036f87b066945
Raiyan <raiyan5764@gmail.com>
2022-01-24 13:18:04 +0600

    bug fixed

--- pages/_app.js
+++ pages/_app.js
@@ -1,33 +1,15 @@
-import { useRouter } from "next/router";
+import Router from "next/router";
 import NProgress from "nprogress";
-import { useEffect } from "react";
 import "../styles/globals.css";
 import "../styles/progress.css";
 
 function MyApp({ Component, pageProps }) {
-  // Router.events.on("routeChangeStart", (url) => {
-  //   NProgress.start();
-  // });
-  // Router.events.on("routeChangeComplete", (url) => {
-  //   NProgress.done();
-  // });
-
-  const router = useRouter();
-
-  useEffect(() => {
-    const handleRouteChange = (url) => {
-      NProgress.start();
-    };
-    const handleRouteChangeOff = (url) => {
-      NProgress.done();
-    };
-
-    router.events.on("routeChangeStart", handleRouteChange);
-
-    return () => {
-      router.events.off("routeChangeStart", handleRouteChangeOff);
-    };
-  }, []);
+  Router.events.on("routeChangeStart", (url) => {
+    NProgress.start();
+  });
+  Router.events.on("routeChangeComplete", (url) => {
+    NProgress.done();
+  });
 
   return (
     <>
